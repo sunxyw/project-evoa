5a38ccfd492f3871e2586252dd80859b0c7acb23
Sunxyw <git@sunxyw.xyz>
2024-09-28 00:42:14 +0800

    feat(ui): add HoverCard component

---
+++ apps/storybook/src/stories/HoverCard.stories.tsx
@@ -0,0 +1,30 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@repo/ui/components/hover-card";
+
+const meta = {
+  title: "HoverCard",
+  component: HoverCard,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {},
+} satisfies Meta<typeof HoverCard>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Basic: Story = {
+  render: () => (
+    <HoverCard>
+      <HoverCardTrigger>Hover</HoverCardTrigger>
+      <HoverCardContent>
+        The React Framework – created and maintained by @vercel.
+      </HoverCardContent>
+    </HoverCard>
+  ),
+};
---
+++ packages/ui/src/components/hover-card.tsx
@@ -0,0 +1,47 @@
+import { cn } from "@/lib/utils";
+import {
+  Content,
+  type HoverCardProps,
+  Root,
+  Trigger,
+} from "@radix-ui/react-hover-card";
+import {
+  forwardRef,
+  type ElementRef,
+  type ComponentPropsWithoutRef,
+} from "react";
+
+const HoverCard = ({
+  openDelay = 50,
+  closeDelay = 100,
+  ...props
+}: HoverCardProps) => (
+  <Root openDelay={openDelay} closeDelay={closeDelay} {...props} />
+);
+
+const HoverCardTrigger = Trigger;
+
+const HoverCardContent = forwardRef<
+  ElementRef<typeof Content>,
+  ComponentPropsWithoutRef<typeof Content>
+>(
+  (
+    { className, align = "center", side = "top", sideOffset = 10, ...props },
+    ref,
+  ) => (
+    <Content
+      ref={ref}
+      align={align}
+      side={side}
+      sideOffset={sideOffset}
+      className={cn(
+        "z-50 w-64 rounded-md border bg-popover p-4 text-popover-foreground shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+        className,
+      )}
+      {...props}
+    />
+  ),
+);
+HoverCardContent.displayName = Content.displayName;
+
+export { HoverCard, HoverCardTrigger, HoverCardContent };
